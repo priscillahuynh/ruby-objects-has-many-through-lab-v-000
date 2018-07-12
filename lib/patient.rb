b43fba4d288920711fac0bfa47eb8f66304da318
Priscilla H <priscillamhuynh@gmail.com>
2018-07-12 12:28:26 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,24 +4,12 @@ class Patient
 
   def initialize(name)
     @name=name
-    @appointments=[]
     @@all<<self
   end
 
-  def new_appointment(doctor,date)
-    appointment=Appointment.new(self, doctor, date)
-    appointment.patient=self
-    @appointments<<appointment
+  def patient(doctor,date)
+    appointment=Appointment.new(self,doctor,date)
+    @@all<<appointment
     appointment
-  end
-
-  def appointments
-    @appointments
-  end
-
-  def doctors
-    @appointments.collect do |appointments|
-      appointments.doctor
-    end
   end
 end
